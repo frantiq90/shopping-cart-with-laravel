8b7f68b54f99d71fc90ef8fbb7973893865f377c
frantiq90 <oscar.kosela@gmail.com>
2017-08-01 19:41:47 +0000

    products thumnails view

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,10 +4,17 @@
     <meta charset="UTF-8">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+    <link rel="stylesheet" href="{{ URL::to('css/shopping-cart.css') }}">
     @yield('styles')
 </head>
 <body>
-@yield('content')
+@include('partials.header')
+
+<div class="container">
+    @yield('content')
+</div>
+
 <script src="http://code.jquery.com/jquery-1.12.4.min.js"
         integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
         crossorigin="anonymous"></script>
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -5,5 +5,91 @@
 @endsection
 
 @section('content')
-    <h1>OK</h1>
+    <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img class="img-responsive" src="https://www.sorbet.co.za/wp-content/uploads/2016/02/bathandbody_300x300-1.jpg" alt="...">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="description">Some description about product</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">150 PLN</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Basket</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img class="img-responsive" src="https://www.sorbet.co.za/wp-content/uploads/2016/02/bathandbody_300x300-1.jpg" alt="...">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="description">Some description about product</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">150 PLN</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Basket</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img class="img-responsive" src="https://www.sorbet.co.za/wp-content/uploads/2016/02/bathandbody_300x300-1.jpg" alt="...">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="description">Some description about product</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">150 PLN</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Basket</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img class="img-responsive" src="https://www.sorbet.co.za/wp-content/uploads/2016/02/bathandbody_300x300-1.jpg" alt="...">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="description">Some description about product</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">150 PLN</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Basket</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img class="img-responsive" src="https://www.sorbet.co.za/wp-content/uploads/2016/02/bathandbody_300x300-1.jpg" alt="...">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="description">Some description about product</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">150 PLN</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Basket</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img class="img-responsive" src="https://www.sorbet.co.za/wp-content/uploads/2016/02/bathandbody_300x300-1.jpg" alt="...">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p class="description">Some description about product</p>
+                    <div class="clearfix">
+                        <div class="pull-left price">150 PLN</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Basket</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
